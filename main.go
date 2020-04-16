f74841f13e5dec297788b644743e58edab197427
Sharad Gupta <bracket@Dragonfly.local>
2020-04-16 22:35:19 +0530

    Final commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/russross/blackfriday"
@@ -9,9 +10,12 @@ import (
 func main() {
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	fmt.Println("Server is running at localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
 
+// GenerateMarkdown generates the markdown
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
 	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
 	rw.Write(markdown)
